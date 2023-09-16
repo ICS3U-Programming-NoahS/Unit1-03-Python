4b5cd3bf3862827099b0c2627227faee39652efc
Noah Smith <noah.smith@stu.ocsb.ca>
2023-09-16 23:56:30 +0000

    Created hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Noah Smith
+# Date: Sep. 16, 2023
+# This program displays Hello, World! with proper style.
+
+
+def main():
+    print("Hello, World! With proper style!")
+    print("I am testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
